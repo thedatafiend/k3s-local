318b1b97fef0a1d160366bfbc589c840dab2dd48
Ryan Russon <ryan.russon@gmail.com>
2022-10-02 19:09:01 -0600

    update installation script

--- installation/k3s-installation.sh
+++ installation/k3s-installation.sh
@@ -7,7 +7,6 @@ echo -e "You have chosen a(n) $insttype installation; proceeding with the setup"
 #   https://rancher.com/docs/k3s/latest/en/installation/install-options/#options-for-installation-with-script 
 
 export INSTALL_K3S_SKIP_DOWNLOAD=false
-# export INSTALL_K3S_SYMLINK=
 export INSTALL_K3S_SKIP_ENABLE=false
 export INSTALL_K3S_SKIP_START=false
 export INSTALL_K3S_VERSION="v1.22.15+k3s1"
@@ -15,13 +14,18 @@ export INSTALL_K3S_BIN_DIR=/usr/local/bin
 export INSTALL_K3S_BIN_DIR_READ_ONLY=false
 export INSTALL_K3S_SYSTEMD_DIR=/etc/systemd/system
 export INSTALL_K3S_EXEC=$insttype
+
+# Keep these as the defaults
+# export INSTALL_K3S_SYMLINK=
 # export INSTALL_K3S_NAME=
 # export INSTALL_K3S_TYPE=
 # export INSTALL_K3S_SKIP_SELINUX_RPM=
 # export INSTALL_K3S_CHANNEL_URL=
-#export INSTALL_K3S_CHANNEL=
+# export INSTALL_K3S_CHANNEL=
 
 echo -e "Beginning to install k3s using the script installation method ";
-instlink="https://rancher.com/docs/k3s/latest/en/installation/install-options/#options-for-installation-with-script";
+instdoc="https://rancher.com/docs/k3s/latest/en/installation/install-options/#options-for-installation-with-script";
 echo -e "For more information on this installation method, see the following:"; 
-echo -e "$instlink";
+echo -e "$instdoc";
+
+curl -sfL https://get.k3s.io | sh -
